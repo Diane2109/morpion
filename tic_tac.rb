52ff9a96715f3e476501e25a4af8738ccbf18c08
Diane2109 <40659353+Diane2109@users.noreply.github.com>
2018-07-18 00:02:41 +0200

    Update tic_tac.rb

--- tic_tac.rb
+++ tic_tac.rb
@@ -95,6 +95,7 @@ class Game # Je détermine la class Game qui permet de lancer la totalité du je
       winner
     end
   end
+ end
 
   def winner # Je détermine la méthode qui permet de savoir si on gagne ou si on fait match nul
     if (@board[1] == @board[2] && @board[2] == @board[3] && @board[1] != " ") || (@board[1] == @board[4] && @board[4] == @board[7] && @board[1] != " ") || (@board[1] == @board[5] && @board[5] == @board[9] && @board[1] != " ") || (@board[2] == @board[5] && @board[5] == @board[8] && @board[2] != " ") || (@board[3] == @board[6] && @board[6] == @board[9] && @board[3] != " ") || (@board[4] == @board[5] && @board[5] == @board[6] && @board[4] != " ") || (@board[7] == @board[8] && @board[8] == @board[9] && @board[7] != " ") || (@board[3] == @board[5] && @board[5] == @board[7] && @board[3] != " ") && @turn % 2 == 0
